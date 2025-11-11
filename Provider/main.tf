1d7515b70cf89c640ff05fda4bdd094371e66ac2
Raunak Sharma <raunakmatrix87@gmail.com>
2025-11-11 12:21:54 +0530

    roles

--- Provider/main.tf
+++ Provider/main.tf
@@ -14,3 +14,21 @@ resource "btp_subaccount_role_collection_assignment" "terraform_dev" {
   role_collection_name = "Subaccount Viewer"
   user_name            = "mujeeb.c@motiveminds.com"
 }
+
+resource "btp_subaccount_role_collection_assignment" "Destination_Administrator" {
+  subaccount_id        = "3d4eaff7-6bf1-4dd8-a94d-c39cb5abb8a6"
+  role_collection_name = "Destination Administrator"
+  user_name            = "mujeeb.c@motiveminds.com"
+}
+
+resource "btp_subaccount_role_collection_assignment" "Connectivity_Destination_Administrator" {
+  subaccount_id        = "3d4eaff7-6bf1-4dd8-a94d-c39cb5abb8a6"
+  role_collection_name = "Connectivity and Destination Administrator"
+  user_name            = "mujeeb.c@motiveminds.com"
+}
+
+resource "btp_subaccount_role_collection_assignment" "Cloud_Connector_Administrator" {
+  subaccount_id        = "3d4eaff7-6bf1-4dd8-a94d-c39cb5abb8a6"
+  role_collection_name = "Cloud Connector Administrator"
+  user_name            = "mujeeb.c@motiveminds.com"
+}
