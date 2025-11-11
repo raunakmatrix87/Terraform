4d496789824f545a585ba98bbeda97cb72a0b131
Raunak Sharma <raunakmatrix87@gmail.com>
2025-11-11 12:00:35 +0530

    sub-account role assignment

--- Provider/main.tf
+++ Provider/main.tf
@@ -8,3 +8,9 @@ resource "btp_globalaccount_role_collection_assignment" "jd" {
   role_collection_name = "Global Account Viewer"
   user_name            = "mujeeb.c@motiveminds.com"
 }
+
+resource "btp_subaccount_role_collection_assignment" "terraform_dev" {
+  subaccount_id        = "3d4eaff7-6bf1-4dd8-a94d-c39cb5abb8a6"
+  role_collection_name = "Subaccount Viewer"
+  user_name            = "mujeeb.c@motiveminds.com"
+}
